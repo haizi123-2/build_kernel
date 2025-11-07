e6d6766a33966fd1702516289bcefc8101fabcbf
Child I <haiziji123@gmail.com>
2025-11-07 18:28:52 +0800

    Update build_tool.sh

--- build_tool.sh
+++ build_tool.sh
@@ -48,7 +48,7 @@ clean(){
 build_kernel(){
 	export KBUILD_BUILD_USER="zahi0"  
 	export KBUILD_BUILD_HOST="zahi0-server"  
-	make $args ineage_ oneplus5_defconfig #修改配置文件
+	make $args lineage_ oneplus5_defconfig #修改配置文件
 	if [ $? -ne 0 ]; then
       		exit 0
         fi
